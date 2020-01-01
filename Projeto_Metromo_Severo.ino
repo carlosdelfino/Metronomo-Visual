0221d34d87d3989460f9ecb50818392a92bcdca9
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2019-12-31 21:03:39 -0300

    melhorias no display e metronomo
    Foi corrigido a exibicao do BPM
    Foi proposto um algoritmo para o metronomo, ainda nao Ã© o solicitado.

--- Projeto_Metromo_Severo.ino
+++ Projeto_Metromo_Severo.ino
@@ -37,33 +37,55 @@
 
 */
 
-#define BLINK_LCD false
+#define BLINK_LCD    false
+
+#define POT_VCC      A1
+#define POT_GND      A3
+#define POT_DERIVADA A2
+
+#define BUTTON_PORT  A0
 
 // BPM máximo e mínimo
-#define BPM_MAX 360
-#define BPM_MIN 30
+#define BPM_MAX      360
+#define BPM_MIN      30
+
+#define LED1         13
+#define LED2         12
+#define LED3         11
+#define LED4          3
 
 // NOMES DOS BOTÕES
-#define BAIXO 1
-#define CIMA 2
-#define ESQUERDA 3
-#define DIREITA 4
-#define SELECTE 5
+#define BAIXO         1
+#define CIMA          2
+#define ESQUERDA      3
+#define DIREITA       4
+#define SELECIONA     5
+
+#define LCD_RS        8
+#define LCD_ENABLE    9
+#define LCD_D4        4
+#define LCD_D5        5
+#define LCD_D6        6
+#define LCD_D7        7
 
 // include the library code:
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
-int bpm = 30;
+double bpm = 30;
+double bpmShow;
 
-const double time_blink = 700;
+const double TIME_BLINK = 700;
 double last_blink = 0;
 
-double last_led = 0;
+double last_led1 = 0;
+double last_led2 = 0;
+double last_led3 = 0;
+double last_led4 = 0;
 
-const double TIME_BUTTON = 250;
+const double TIME_BUTTON = 350;
 double last_cima = 0;
 double last_baixo = 0;
 
@@ -76,47 +98,69 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print("BPM :");
 
+  pinMode(POT_VCC, OUTPUT);
+  pinMode(POT_GND, OUTPUT);
+  digitalWrite(POT_VCC, LOW);
+  digitalWrite(POT_GND, HIGH);
 
-  pinMode(A1, OUTPUT);
-  pinMode(A3, OUTPUT);
-  digitalWrite(A1, LOW);
-  digitalWrite(A3, HIGH);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
 
-  pinMode(13, OUTPUT);
-  pinMode(11, OUTPUT);
-
-  digitalWrite(13, LOW);
-  digitalWrite(11, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
 
 }
 
 void loop() {
-  analogWrite(11, map(analogRead(A2), 0, 1024, 0, 255) );
+  //  analogWrite(LED3, map(analogRead(POT_DERIVADA), 0, 1024, 0, 255) );
 
-if(BLINK_LCD)  blinkLCD();
-
-  lcd.setCursor(5, 1);
-  lcd.print("             ");
-  lcd.setCursor(5, 1);
+#if BLINK_LCD
+  blinkLCD();
+#endif
 
   byte button = readButton();
 
   checaBPM(button);
 
-  lcd.print(bpm);
-
-lcd.print("##");
-  lcd.print(analogRead(A0));
+  showBPM();
 
   piscaTodos();
 }
 
-void piscaTodos() {
-  if (millis() - last_led >= bpm * 60 * 1000 / 2) {
-    digitalWrite(13, !digitalRead(13));
-    last_led = millis();
+void showBPM(){
+  if(bpmShow != bpm){
+  lcd.setCursor(5, 1);
+  lcd.print("    ");
+  lcd.setCursor(5, 1);
+  lcd.print(bpm);
+  bpmShow = bpm;
   }
 
+}
+
+void piscaTodos() {
+  static byte ledState = 1;
+  double bpmCalc = (bpm / 60) * 1000;
+      if (millis() - last_led4 >= bpmCalc / 2) {
+        digitalWrite(LED4, digitalRead(LED1));
+        last_led4 = millis();
+      }
+      if (millis() - last_led3 >= bpmCalc / 3) {
+        digitalWrite(LED3, digitalRead(LED1));
+        last_led3 = millis();
+      }
+      if (millis() - last_led2 >= bpmCalc / 4) {
+        digitalWrite(LED2, digitalRead(LED1));
+        last_led2 = millis();
+      }
+      if (millis() - last_led1 >= bpmCalc) {
+        digitalWrite(LED1, !digitalRead(LED1));
+        last_led1 = millis();
+      }
 }
 
 void checaBPM(byte button) {
@@ -138,7 +182,7 @@ void checaBPM(byte button) {
   }
 }
 byte readButton() {
-  int  botao = analogRead (A0);  //Leitura do valor da porta analógica A0
+  int  botao = analogRead (BUTTON_PORT);  //Leitura do valor da porta analógica A0
   if (botao < 100) {
     return DIREITA;
   }   else if (botao < 200) {
@@ -148,14 +192,14 @@ byte readButton() {
   }   else if (botao < 600) {
     return ESQUERDA;
   }   else if (botao < 800) {
-    return SELECTE;
+    return SELECIONA;
   }
   return NULL;
 }
 
 void blinkLCD() {
   static boolean lcd_display = false;
-  if (millis() - last_blink >= time_blink) {
+  if (millis() - last_blink >= TIME_BLINK) {
     if (lcd_display) {
       lcd.noDisplay();
       lcd_display = false;
